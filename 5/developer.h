58aec8e79e733ca188067ee4ceb758e213db675c
Conrad Indiono <conrad@indygemma.com>
2026-03-04 16:36:11 +0100

    update developer

--- 5/developer.h
+++ 5/developer.h
@@ -1,1 +1,13 @@
+#ifndef __DEVELOPER__
+#define __DEVELOPER__
 
+#include "mitarbeiter.h"
+
+class Developer : public Mitarbeiter {
+public:
+  Developer(int gehalt, std::string name);
+
+  virtual bool work(int hours) override;
+};
+
+#endif
---
+++ 5/main.cpp
@@ -0,0 +1,9 @@
+#include "developer.h"
+#include <iostream>
+
+int main() {
+  std::cout << "Developer: A. King" << std::endl;
+  Developer dev1(5500, "A. King");
+  dev1.work(15);
+  return 0;
+}
