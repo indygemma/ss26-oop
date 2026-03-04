6af4c0d5a1ac8586d0f7dbb098cbc4c12c926a39
Conrad Indiono <conrad@indygemma.com>
2026-03-04 17:23:08 +0100

    slicing problem showcased

--- 5/main.cpp
+++ 5/main.cpp
@@ -8,5 +8,29 @@ int main() {
   Marketing marketing1(5000, "C. Tibor");
   dev1.work(15);
   marketing1.work(20);
+
+  std::cout << "Sizes of Classes: " << std::endl;
+  std::cout << "Mitarbeiter sizeof: " << sizeof(Mitarbeiter) << std::endl;
+  std::cout << "Developer sizeof: " << sizeof(Developer) << std::endl;
+  std::cout << "Marketing sizeof: " << sizeof(Marketing) << std::endl;
+
+  Developer *dev2 = new Developer(7000, "B. Cross", "F#");
+
+  // SLICING PROBLEM (without pointers)
+  std::cout << "Let all work for 40 hours (polymorphic). SLICING PROBLEM"
+            << std::endl;
+  std::vector<Mitarbeiter> xs = {dev1, marketing1, *dev2};
+  for (int i = 0; i < 3; i++) {
+    xs[i].work(40);
+  }
+
+  // SLICING PROBLEM SOLVED (with pointers)
+  std::cout << "Let all work for 40 hours (polymorphic). SLICING PROBLEM SOLVED"
+            << std::endl;
+  std::vector<Mitarbeiter *> xs1 = {&dev1, &marketing1, dev2};
+  for (int i = 0; i < 3; i++) {
+    xs1[i]->work(40);
+  }
+
   return 0;
 }
