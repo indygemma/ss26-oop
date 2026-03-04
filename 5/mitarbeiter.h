da9176ec7afbcdc29f2ed235dfd3821e07f39b5a
Conrad Indiono <conrad@indygemma.com>
2026-03-04 16:30:39 +0100

    start inheritance example

---
+++ 5/mitarbeiter.h
@@ -0,0 +1,20 @@
+#ifndef __MITARBEITER__
+#define __MITARBEITER__
+
+#include <string>
+
+class Mitarbeiter {
+private:
+  int gehalt; // in euro
+protected:
+  std::string name;
+
+public:
+  Mitarbeiter(int gehalt, std::string name);
+  virtual ~Mitarbeiter(); // als Parentklasse, muss Destruktor als virtual
+                          // deklariert werden.
+
+  virtual bool work(int hours) = 0;
+};
+
+#endif
