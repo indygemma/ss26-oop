da4d50c61a8ea0c3756aefd2fac57148f07ffdad
Conrad Indiono <conrad@indygemma.com>
2026-04-08 16:29:10 +0200

    01_addition.cpp - templates

---
+++ 13/01_addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace our_functions {
+// int add(int x, int y) { return x + y; }
+// double add(double x, double y) { return x + y; }
+// string add(string x, string y) { return x + y; }
+
+template <typename T> T add(T x, T y) { return x + y; }
+} // namespace our_functions
+
+int main() {
+  cout << our_functions::add(1, 2) << endl;
+  cout << our_functions::add(1.5, 2.6) << endl;
+  cout << our_functions::add<string>("string x", "string y") << endl;
+}
