ee60b75f1836aafaeb100b1a00b04f5e5270bccd
Conrad Indiono <conrad@indygemma.com>
2026-04-23 10:44:35 +0200

    implement LiveStream::start

--- 18/livestream.cpp
+++ 18/livestream.cpp
@@ -8,6 +8,19 @@ LiveStream::LiveStream(std::string name, int initialViewers,
       viewerFluctuation(viewerFluctuation) {}
 
 void LiveStream::start() {
-  // TODO: implement Livestream start
+  // DONE: implement Livestream start
   cout << name << " starts with " << viewerCount << " initial viewers." << endl;
+  size_t i = 0;
+  size_t maxViewerCount = viewerCount;
+  while (viewerCount > 0) {
+    cout << name << "running with " << viewerCount << " active viewers."
+         << endl;
+    viewerCount = viewerCount + viewerFluctuation - i;
+    if (viewerCount > maxViewerCount) {
+      maxViewerCount = viewerCount;
+    }
+    i++;
+  }
+  cout << name << " ended with a maximum of " << maxViewerCount
+       << " concurrent viewers." << endl;
 }
