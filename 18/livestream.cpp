060bf7b2e04a39f197c6bf3354b70a5554f79a8f
Conrad Indiono <conrad@indygemma.com>
2026-04-23 10:32:32 +0200

    add livestream missing definitions, add startAllStreams in main

--- 18/livestream.cpp
+++ 18/livestream.cpp
@@ -4,3 +4,7 @@ LiveStream::LiveStream(std::string name, int initialViewers,
                        int viewerFluctuation)
     : Stream(name), viewerCount(initialViewers),
       viewerFluctuation(viewerFluctuation) {}
+
+void LiveStream::start() {
+  // TODO: implement Livestream start
+}
--- 18/main.cpp
+++ 18/main.cpp
@@ -4,5 +4,7 @@
 int main() {
   Host host;
   host.addStream(new LiveStream("Formula 1", 100, 300));
+  host.addStream(new LiveStream("Formula 2", 10, 50));
+  host.startAllStreams();
   return 0;
 }
