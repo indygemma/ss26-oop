71750e5c8604700013773aa88a7d39d5b64309e1
Conrad Indiono <conrad@indygemma.com>
2026-05-13 17:36:11 +0200

    completed all methods + tests using catch

--- 22/UniquePointer.h
+++ 22/UniquePointer.h
@@ -19,10 +19,37 @@ public:
     }
   }
 
+  // MOVE
   UniquePointer(UniquePointer &&other) : ptr(other.ptr) { other.ptr = nullptr; }
+  UniquePointer &operator=(UniquePointer &&other) {
+    if (ptr != other.ptr) {
+      delete ptr;
+      ptr = other.ptr;
+      other.ptr = nullptr;
+    }
+    return *this;
+  }
 
   T operator*() { return *ptr; }
+  T *operator->() { return ptr; }
   operator bool() const { return ptr != nullptr; }
+
+  void reset(T *ptr = nullptr) {
+    delete this->ptr;
+    this->ptr = ptr;
+  }
+
+  T *release() {
+    T *tmp = ptr;
+    ptr = nullptr;
+    return tmp;
+  }
+
+  void swap(UniquePointer<T> &other) {
+    T *tmp = ptr;
+    ptr = other.ptr;
+    other.ptr = tmp;
+  }
 };
 
 #endif
--- 22/unique_ptr_test.cpp
+++ 22/unique_ptr_test.cpp
@@ -23,3 +23,40 @@ TEST_CASE("UniquePointer Move-Constructor works") {
   REQUIRE_FALSE(ptr1);
   REQUIRE(*ptr2 == 100);
 }
+
+TEST_CASE("UniquePointer Move-Assignment works") {
+  UniquePointer<int> a(new int(1));
+  UniquePointer<int> b(new int(2));
+
+  b = std::move(a);
+
+  REQUIRE_FALSE(a);
+  REQUIRE(*b == 1);
+}
+
+TEST_CASE("UniquePointer reset") {
+  UniquePointer<int> ptr(new int(100));
+  ptr.reset(new int(5000));
+
+  REQUIRE(*ptr == 5000);
+}
+
+TEST_CASE("UniquePointer release: not owned anymore, no delete") {
+  UniquePointer<int> ptr(new int(100));
+  int *x = ptr.release();
+
+  REQUIRE_FALSE(ptr);
+  REQUIRE(*x == 100);
+}
+
+TEST_CASE("UniquePointer swap: between two UniquePointers") {
+  UniquePointer<int> a(new int(10));
+  UniquePointer<int> b(new int(20));
+
+  a.swap(b);
+
+  REQUIRE(a);
+  REQUIRE(b);
+  REQUIRE(*a == 20);
+  REQUIRE(*b == 10);
+}
